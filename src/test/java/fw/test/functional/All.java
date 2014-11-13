2c2891bf88896f75dab98655c41c2f456f54a8da
pengyi <ryanpoy@gmail.com>
2014-11-13 14:29:14 +0800

    add junit maven dependency

--- src/test/java/fw/test/functional/All.java
+++ src/test/java/fw/test/functional/All.java
@@ -3,7 +3,7 @@ package fw.test.functional;
 import junit.framework.TestSuite;
 import junit.textui.TestRunner;
 
-public class all {
+public class All {
 
 	public static void main(String[] args) {
 		TestRunner runner = new TestRunner();
