a095c49e630b6a9f8ec8dfd30f623b492189eacf
pengyi <ryanpoy@gmail.com>
2015-02-14 03:16:02 +0800

    恢复webContextHandler

--- src/main/java/com/hunantv/fw/Application.java
+++ src/main/java/com/hunantv/fw/Application.java
@@ -214,33 +214,34 @@ public class Application {
 		return httpConfiguration;
 	}
 	
-//	public void start() throws Exception {
-//		WebAppContext webAppCtx = new WebAppContext();
-//		webAppCtx.setContextPath("/");
-//		webAppCtx.addServlet(new ServletHolder(new Dispatcher()), "/*");
-//		/*
-//		 * String projectPath = new String("/Users/ryan/Workspace/happy_sunshine");
-//		 * webAppCtx.setDefaultsDescriptor(projectPath + "/demo-war/target/jetty-demo-war/WEB-INF/web.xml");
-//		 * webAppCtx.setResourceBase(projectPath + "/demo-war/target/jetty-demo-war");
-//		 */
-//		webAppCtx.setDefaultsDescriptor("");
-//		webAppCtx.setResourceBase("");
-//		webAppCtx.setParentLoaderPriority(true);
-//		this.server.setHandler(webAppCtx);
-//
-//		logger.info("application listen on " + port);
-//		this.server.start();
-//		this.server.join();
-//	}
 	public void start() throws Exception {
-	    ServletHandler handler = new ServletHandler();
-	    server.setHandler(handler);
-	    handler.addServletWithMapping(Dispatcher.class, "/*");
-	    
-	    logger.info("application listen on " + port);
+		WebAppContext webAppCtx = new WebAppContext();
+		webAppCtx.setContextPath("/");
+		webAppCtx.addServlet(new ServletHolder(new Dispatcher()), "/*");
+		/*
+		 * String projectPath = new String("/Users/ryan/Workspace/happy_sunshine");
+		 * webAppCtx.setDefaultsDescriptor(projectPath + "/demo-war/target/jetty-demo-war/WEB-INF/web.xml");
+		 * webAppCtx.setResourceBase(projectPath + "/demo-war/target/jetty-demo-war");
+		 */
+		webAppCtx.setDefaultsDescriptor("");
+		webAppCtx.setResourceBase("");
+		webAppCtx.setParentLoaderPriority(true);
+		this.server.setHandler(webAppCtx);
+
+		logger.info("application listen on " + port);
 		this.server.start();
 		this.server.join();
 	}
+	
+//	public void start() throws Exception {
+//	    ServletHandler handler = new ServletHandler();
+//	    server.setHandler(handler);
+//	    handler.addServletWithMapping(Dispatcher.class, "/*");
+//	    
+//	    logger.info("application listen on " + port);
+//		this.server.start();
+//		this.server.join();
+//	}
 
 	public void stop() throws Exception {
 		this.server.stop();
