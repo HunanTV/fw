af14e3bc8ae291f974384ab1ee7fa0748c3d5dba
pengyi <ryanpoy@gmail.com>
2015-02-15 17:08:25 +0800

    添加了获取远程IP的接口

--- src/main/java/com/hunantv/fw/utils/WebUtil.java
+++ src/main/java/com/hunantv/fw/utils/WebUtil.java
@@ -8,4 +8,21 @@ public class WebUtil {
 		String contentType = request.getContentType();
 		return contentType != null && contentType.startsWith("multipart/form-data");
 	}
+
+	private static String[] IP_HEADS = new String[] { 
+		"X-Forwarded-For", 
+		"Proxy-Client-IP", 
+		"WL-Proxy-Client-IP",
+	    "HTTP_CLIENT_IP", 
+	    "HTTP_X_FORWARDED_FOR",
+	};
+
+	public static String getRemoteAddr(HttpServletRequest request) {
+		for (String header : IP_HEADS) {
+			String ip = request.getHeader(header);
+			if (ip != null && ip.length() > 0 && !"unknown".equalsIgnoreCase(ip))
+				return ip;
+		}
+		return request.getRemoteAddr();
+	}
 }
