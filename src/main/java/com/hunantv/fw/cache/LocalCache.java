a7db8143829a8247ec02f092891d6b0c4332ead5
pengyi <ryanpoy@gmail.com>
2015-04-02 15:48:34 +0800

    加入了LocalCache的配置文件

---
+++ src/main/java/com/hunantv/fw/cache/LocalCache.java
@@ -0,0 +1,73 @@
+package com.hunantv.fw.cache;
+
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import com.hunantv.fw.utils.FwLogger;
+import com.hunantv.fw.utils.SysConf;
+
+public class LocalCache {
+	public static final FwLogger logger = new FwLogger(LocalCache.class);
+	public static int LRU_MAX_LEN = 1024;
+	public static long LRU_EXPIRED_SECONDS = 0L;
+
+	private ConcurrentMap<String, LRU> caches = new ConcurrentHashMap<String, LRU>();
+
+	private static LocalCache lc = null;
+
+	private LocalCache() {
+		try {
+			SysConf conf = new SysConf();
+			Properties pros = conf.read("cache.properties");
+			Object obj = pros.get("cache.local.length");
+			if (null != obj) {
+				LocalCache.LRU_MAX_LEN = Integer.valueOf(((String) obj).trim());
+			}
+			logger.debug("Local Cache Max Length: " + LocalCache.LRU_MAX_LEN);
+			obj = pros.get("cache.local.expired");
+			if (null != obj) {
+				LocalCache.LRU_EXPIRED_SECONDS = Long.valueOf(((String) obj).trim());
+			}
+			logger.debug("Local Cache Default Expired Seconds: " + LocalCache.LRU_EXPIRED_SECONDS);
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+	public static LocalCache instance() {
+		if (null == lc)
+			lc = new LocalCache();
+		return lc;
+	}
+
+	public void set(String tableName, String key, Object value) {
+		LRU lru = caches.get(tableName);
+		if (null == lru) {
+			lru = new LRU(LRU_MAX_LEN);
+			lru.put(key, value, LocalCache.LRU_EXPIRED_SECONDS);
+			caches.put(tableName, lru);
+		} else {
+			lru.put(key, value);
+		}
+	}
+
+	public Object get(String tableName, String key) {
+		LRU lru = caches.get(tableName);
+		return null == lru ? null : lru.get(key);
+	}
+
+	public LRU table(String name) {
+		return this.caches.get(name);
+	}
+
+	public void clearAll() {
+		caches.clear();
+	}
+
+	public void clear(String tableName) {
+		if (caches.containsKey(tableName)) {
+			caches.remove(tableName);
+		}
+	}
+}
---
+++ src/test/java/fw/test/unit/cache/LocalCacheTest.java
@@ -0,0 +1,38 @@
+package fw.test.unit.cache;
+
+import junit.framework.TestCase;
+
+import com.hunantv.fw.cache.LocalCache;
+
+public class LocalCacheTest extends TestCase {
+
+	private String tbname = "bigdata";
+
+	public void setUp() {
+		LocalCache.instance().clear(tbname);
+	}
+
+	public void tearDown() {
+		LocalCache.instance().clear(tbname);
+	}
+
+	public void testNotInCache() {
+		Object obj = LocalCache.instance().get(tbname, "key1");
+		assertNull(obj);
+	}
+
+	public void testNotInCacheIfExpired() {
+		LocalCache lc = LocalCache.instance();
+		LocalCache.LRU_EXPIRED_SECONDS = 1L;
+		lc.set(tbname, "key1", "123456");
+		String value = (String) lc.get(tbname, "key1");
+		assertEquals("123456", value);
+		try {
+			Thread.sleep(2000);
+			assertNull(lc.get(tbname, "key1"));
+			assertEquals(0, lc.table(tbname).size());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
