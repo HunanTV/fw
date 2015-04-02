0b07653dfb9573b279663cb075ec5c94cf517ef5
pengyi <ryanpoy@gmail.com>
2015-04-02 15:49:18 +0800

    实现了LocalCache的测试

--- src/test/java/fw/test/All.java
+++ src/test/java/fw/test/All.java
@@ -4,6 +4,7 @@ import junit.framework.TestSuite;
 import junit.textui.TestRunner;
 import fw.test.functional.FooControllerTest;
 import fw.test.unit.cache.LRUTest;
+import fw.test.unit.cache.LocalCacheTest;
 import fw.test.unit.view.ViewTest;
 
 public class All {
@@ -14,6 +15,7 @@ public class All {
 		suite.addTestSuite(FooControllerTest.class);
 		suite.addTestSuite(ViewTest.class);
 		suite.addTestSuite(LRUTest.class);
+		suite.addTestSuite(LocalCacheTest.class);
 		runner.run(suite);
 	}
 }
