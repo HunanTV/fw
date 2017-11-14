19513c6311ed700974b393fe9386839dca460bcf
xuyanbo <xuyanbo@e.hunantv.com>
2017-11-14 17:33:14 +0800

    set default env = dev, relocate confs

--- src/demo/java/user/UserController.java
+++ src/demo/java/user/UserController.java
@@ -24,6 +24,7 @@ public class UserController extends Controller {
 	}
 
 	public View HelloWorld() {
+		logger.info("test : {}|{}", "hahaha", 111);
 		return this.renderString("Hello, World");
 	}
 
